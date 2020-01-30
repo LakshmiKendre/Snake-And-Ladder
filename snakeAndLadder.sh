1996447fcf10685784f38a86654826ea7cd666df
admin <lakshmi.patil.nine@gmail.com>
2020-01-30 17:28:30 +0530

    Added: function to Check player moves

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,12 +1,36 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome to Snake & Ladder"
 
 START_POSITION=0
+playerPosition=0
 
 dieRoll(){
 randomCheck=$((RANDOM%6 + 1))
 echo $randomCheck
 }
 
-dieRoll
+checkSnakeOrLadder(){
+NO_PLAY=0
+SNAKE=1
+LADDER=2
+
+dieValue=$(dieRoll)
+option=$((RANDOM%3))
+
+	case $option in
+	$NO_PLAY)
+		playerPosition=$playerPosition 
+		;;
+	$SNAKE)
+		playerPosition=$(($playerPosition - $dieValue))
+		;;
+	$LADDER)
+		playerPosition=$(($playerPosition + $dieValue))
+		;;
+	esac
+echo $playerPosition
+}
+
+checkSnakeOrLadder 
+
